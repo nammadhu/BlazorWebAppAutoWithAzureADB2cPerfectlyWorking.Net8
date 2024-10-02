2784a4898e37e8046cd88d47134ef7af9c4d8157
nammadhu <nammadhu@outlook.in>
2024-10-02 15:50:46 +0530

    api call invocation from client and resposiveness

--- BlazorWebAppAuto.Client/Program.cs
+++ BlazorWebAppAuto.Client/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWebAppAuto.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddHttpClient("TestAPI", client =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
